bd6eb156065a317f3a99745e172b73f508b699cd
khongtrunght <khongtrunght@gmail.com>
2021-11-26 11:59:08 +0700

    fir error

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,10 @@
 from .cGAN import *
-from .gp_cgan import GPCGAN
-from .gp_cgan_28 import GPCGAN28
 from .gp_cgan_32 import GPCGANBEST
 
 
 gan_models = {
     'CGAN': CGAN,
-    'GP-CGAN': GPCGAN,
-    'GP-CGAN28': GPCGAN28,
+    # 'GP-CGAN': GPCGAN,
+    # 'GP-CGAN28': GPCGAN28,
     'GP-CGAN-best': GPCGANBEST,
 }
